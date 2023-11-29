82d72a4cc7485c2337371c04b21a56fe31d49ecd
shanmuga raja <shunmugaraja@shunmugas-MacBook-Pro.local>
2023-11-29 15:44:56 +0530

    firstcommit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const jwt = require("jsonwebtoken");
+
+mongoose.connect("mongodb+srv://shunmugaraja:shammu@cluster0.0sq8ang.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to MongoDB");
+})
+    .catch((err) => {
+        console.log("Error connecting to MongoDB", err);
+    });
+
+
+app.listen(port, () => {
+    console.log("server is running on port 8000");
+});
+
+
